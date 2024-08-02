cc5b2951684f8005016154607682b6860fa1b0dc
Wendy Office PC <skes4126@gmail.com>
2024-08-02 10:45:54 +0800

    優化程式語言V2 Python 608題答案檔

--- 程式語言 V2答案檔/Python/608.py
+++ 程式語言 V2答案檔/Python/608.py
@@ -2,10 +2,7 @@ score = 0
 input_list = []
 for i in range(10):
     n = int(input())
-    input_list = [x + n for x in input_list]
+    input_list = [x + n for x in input_list if x > 0]
     input_list.append(n)
 
 print('score = ' + str(sum(i > 3 for i in input_list)))
-
-
-
