e82ef9e924abca0cdbdce137eea849a4281367f5
Wendy Office PC <skes4126@gmail.com>
2024-08-08 16:22:50 +0800

    移除程式設計：資料結構答案檔

--- 程式設計：資料結構答案檔/Python/304.py
+++
@@ -1,8 +0,0 @@
-m = int(input())
-set0 = {input() for i in range(m)}
-
-count = 0
-for s in set0:
-    for i in range(1, len(s)//2+1):
-        if s[:i] == s[-i:] and s[i:-i] in set0: count += 1
-print(count)
